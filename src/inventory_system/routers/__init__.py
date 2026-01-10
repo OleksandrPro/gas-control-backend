f3fe82e7b79101f155b01211785f949cfc1b2e48
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-01-10 13:34:02 +0100

    chore: Remove redundant comments and unused files

--- src/inventory_system/routers/__init__.py
+++ src/inventory_system/routers/__init__.py
@@ -2,7 +2,4 @@ from .lookups import lookups_router
 from .cards import cards_router
 from .equipment import equipment_router
 
-# Если в будущем появятся другие роутеры, добавляйте их сюда так же:
-# from .cards import router as cards_router
-
 __all__ = ["lookups_router", "cards_router", "equipment_router"]
