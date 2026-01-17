77b0ed636941d73e06575be231773412f28e4980
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-01-17 23:41:21 +0100

    fix: Add missing columns and rename some of the fields

--- src/inventory_system/schemas/card.py
+++ src/inventory_system/schemas/card.py
@@ -6,19 +6,20 @@ class CardCreateSchema(BaseModel):
     inventory_number: str
     inventory_number_eskd: str
     gas_pipeline_section: str
-    pressure_type: Optional[int] = None
     described_name: str
     build_date_dn: date
     total_length: float
     folder: str
-    
-    property_type_id: int = Field(alias="property_type")
-    district_id: int = Field(alias="district")
-    object_name_id: int = Field(alias="object_name")
-    
-    cut_type_id: Optional[int] = Field(default=None, alias="cut_type")
+    address: str
 
-    model_config = ConfigDict(populate_by_name=True)
+    pressure_type_id: int
+    property_type_id: int
+    district_id: int
+    object_name_id: int
+
+    cut_type_id: Optional[int] = None
+
+    model_config = ConfigDict(from_attributes=True)
 
 class CardUpdateSchema(BaseModel):
     inventory_number: Optional[str] = None
@@ -30,34 +31,25 @@ class CardUpdateSchema(BaseModel):
     total_length: Optional[float] = None
     build_date_dn: Optional[date] = None
     
+    pressure_type_id: Optional[int] = None
     property_type_id: Optional[int] = None
     district_id: Optional[int] = None
     object_name_id: Optional[int] = None
     cut_type_id: Optional[int] = None
 
 class DisplayMainPageCard(BaseModel):
+    id: int
     inventory_number: str
     inventory_number_eskd: str
-
     gas_pipeline_section: str
-
     pressure_type_id: int
-
-    # TODO Clarify what does this column in original table even mean
     described_name: str
-
-    # TODO Clarify what does 'OZ' mean
     build_date_dn: date
-
     property_type_id: int
-
     total_length: float
-
     district_id: int
     object_name_id: int
-
     address: str
-
     folder: str
     cut_type_id: Optional[int] = None
 
