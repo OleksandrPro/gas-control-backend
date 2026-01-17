f578075454a6ce2a0cf4872eb07e5ea6122e8f8f
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-01-17 14:17:43 +0100

    fix: Remove duplicate 'card' prefix from endpoints

--- src/inventory_system/routers/cards.py
+++ src/inventory_system/routers/cards.py
@@ -13,27 +13,27 @@ from inventory_system.repositories.card import CardRepository
 
 cards_router = APIRouter(prefix="/cards", tags=["Cards"])
 
-@cards_router.get("/cards")
+@cards_router.get("")
 async def read_all_cards(session: Annotated[AsyncSession, Depends(get_session)]):
     #TODO later add proper response_model to avoid potential mistakes with auto jsonable_encoder
     db_manager = DatabaseManager(session)
     cards = await db_manager.get_all_cards()
     return cards
 
-@cards_router.get("/cards/{id}")
+@cards_router.get("/{id}")
 async def read_card(id: int, session: Annotated[AsyncSession, Depends(get_session)]):
     #TODO later add proper response_model to avoid potential mistakes with auto jsonable_encoder
     db_manager = DatabaseManager(session)
     card = await db_manager.get_card(id)
     return card
 
-@cards_router.post("/cards")
+@cards_router.post("")
 async def create_card(card: DisplayMainPageCard, session: Annotated[AsyncSession, Depends(get_session)]):
     db_manager = DatabaseManager(session)
     repo = CardRepository(db_manager=db_manager)
     return await repo.create(**card.model_dump())
 
-@cards_router.patch("/cards/{id}")
+@cards_router.patch("/{id}")
 async def update_card(id: int, new_data: CardUpdateSchema, session: Annotated[AsyncSession, Depends(get_session)]):
     db_manager = DatabaseManager(session)
     repo = CardRepository(db_manager=db_manager)
@@ -47,7 +47,7 @@ async def update_card(id: int, new_data: CardUpdateSchema, session: Annotated[As
         
     return updated_card
 
-@cards_router.delete("/cards/{card_id}", status_code=status.HTTP_204_NO_CONTENT)
+@cards_router.delete("/{card_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_card(
     card_id: int, 
     session: Annotated[AsyncSession, Depends(get_session)]
--- tests/inventory_system/routers/test_cards.py
+++ tests/inventory_system/routers/test_cards.py
@@ -4,6 +4,6 @@ import json
 
 @pytest.mark.asyncio
 async def test_get_all_cards(test_client):
-    response = await test_client.get("/cards/cards")
+    response = await test_client.get("/cards")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == []
